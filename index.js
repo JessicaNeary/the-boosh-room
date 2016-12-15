742e321e0775c2e3f52ee169fdb7275ee23b398b
David Christie <davidchristie.nz@gmail.com>
2016-12-16 09:25:41 +1300

    Implement basic chat functionality

---
+++ index.js
@@ -0,0 +1,68 @@
+const escape = require('escape-html')
+const express = require('express')
+const handlebars = require('express-handlebars')
+
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+const PORT = process.env.PORT || 3000
+
+// This is the data that is going to be displayed on the page
+// TODO Get this data from a JSON file or database
+const pageData = {
+  characters: [
+    {
+      avatar: '/images/howard-moon.jpg',
+      id: 1,
+      name: 'Howard Moon'
+    },
+    {
+      avatar: '/images/vince-noir.jpg',
+      id: 2,
+      name: 'Vince Noir'
+    },
+    {
+      avatar: '/images/naboo-the-enigma.jpg',
+      id: 3,
+      name: 'Naboo the Enigma'
+    },
+    {
+      avatar: '/images/bollo.jpg',
+      id: 4,
+      name: 'Bollo'
+    },
+    {
+      avatar: '/images/bob-fossil.gif',
+      id: 5,
+      name: 'Bob Fossil'
+    }
+  ]
+}
+
+app.engine('hbs', handlebars({
+  defaultLayout: 'main.hbs',
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index', pageData))
+
+// Handle new connection
+io.on('connection', socket => {
+
+  // Handle recieving message from client
+  socket.on('boosh message', message => {
+
+    // Send the message to all connected clients
+    io.emit('boosh message', {
+      avatar: escape(message.avatar),
+      character: escape(message.character),
+      text: escape(message.text)
+    })
+  })
+})
+
+http.listen(PORT, () => console.log('Listening on port', PORT))
---
+++ public/js/script.js
@@ -0,0 +1,76 @@
+(function () {
+
+  var socket = io()
+
+  // These are set when the user chooses a character from the select character
+  // modal.
+  var character = null // Name of the character
+  var avatar = null // Link to avatar image
+
+  $(document).ready(function () {
+
+    // Show character select modal as soon as the page has finished loading.
+    $('#character-select').modal({
+      backdrop: 'static',
+      keyboard: false
+    })
+
+    // Handle changing to a different character
+    $('#character').change(event => {
+      var avatar = $('#character').find(":selected").attr('avatar')
+      $('#avatar').attr('src', avatar)
+    })
+
+    // Handle clicking okay on character select modal
+    $('#character-select-button').click(function (event) {
+      avatar = $('#avatar').attr('src')
+      character = $('#character').val()
+    })
+  })
+
+  // Handle user submitting input
+  $('#input').submit(function (event) {
+
+    // Stop the form from sending an HTTP request
+    event.preventDefault()
+
+    if ($('#text').val().length === 0)
+      return // No text given - do nothing
+
+    // Send the message to the server
+    var text = $('#text').val()
+    socket.emit('boosh message', {
+      avatar: avatar,
+      character: character,
+      text: text
+    })
+
+    // Clear the text box
+    $('#text').val('')
+  })
+
+  // Handle receiving a message from the server
+  socket.on('boosh message', function (data) {
+
+    var avatar = data.avatar
+    var character = data.character
+    var text = data.text
+
+    // Show the message
+    $('#messages')
+      .append(
+        `<li>
+          <div class="span4">
+            <img class="img-responsive img-rounded" src="${avatar}"/>
+            <div class="content-heading">
+              <h3>${character}</h3>
+              ${text}
+            </div>
+          </div>
+        </li>`)
+
+    // Scroll to the bottom of the page
+    window.scrollTo(0, document.body.scrollHeight)
+  })
+
+})()
